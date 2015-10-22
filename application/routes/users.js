0fb8c2ee5dfdefed137dd13110b933fb90e211bd
Sreekanth Asodi <sreekanth.asodi@invati-insights.com>
2015-10-22 22:37:26 +0530

    just a simple api that returns a userlist from mongo and adds users

---
+++ application/routes/users.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.find({},{},function(e,docs){
+        res.json(docs);
+    });
+});
+
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+
+module.exports = router;
